4d7005945d9072a3a8e538c65bf5f8e24f601488
ikyrie <mah.c5r@gmail.com>
2023-09-26 14:28:06 -0300

    Criando as lógicas de criar uma mensagem, criar uma bolha bot e uma bolha usuario

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let chat = document.querySelector('#chat');
+let input = document.querySelector('#input');
+let botaoEnviar = document.querySelector('#botao-enviar');
+
+function enviarMensagem() {
+    if(input.value == "" || input.value == null) return;
+    let message = input.value;
+    let novaBolha = criaBolhaUsuario();
+    novaBolha.innerHTML = message;
+    chat.appendChild(novaBolha);
+    input.value = "";
+}
+
+function criaBolhaUsuario() {
+    let bolha = document.createElement('p');
+    bolha.classList = 'chat__bolha chat__bolha--usuario';
+    return bolha;
+}
+
+function criaBolhaBot() {
+    let bolha = document.createElement('p');
+    bolha.classList = 'chat__bolha chat__bolha--bot';
+    return bolha;
+}
+
+botaoEnviar.addEventListener('click', enviarMensagem);
